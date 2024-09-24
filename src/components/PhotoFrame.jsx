a04bd40eee6fad8ed830364400852126b697f4e8
aneeshpatne <aneeshpatne@gmail.com>
2024-09-24 11:26:45 +0530

    pic update

--- src/components/PhotoFrame.jsx
+++ src/components/PhotoFrame.jsx
@@ -6,7 +6,7 @@ export default function Photoframe(){
     const {theme, ThemeToggle} = UseThemeContext();
     return(<div id={styles.PhotoFrameContainer} className ={theme === 'LightMode' ? styles.LightMode : ''}>
         <div id={styles.FrameImgContainer}>
-            <Image src='/assets/img/aneesh.png' alt='its me' height={640} width={640} draggable={false}/>
+            <Image src='/assets/img/aneesh3.png' alt='its me' height={640} width={640} draggable={false} style={{ objectFit: 'contain' }}/>
         </div>
     </div>);
 }
