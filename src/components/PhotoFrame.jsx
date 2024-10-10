9a79ae01ec939e275c87729c975753fc2d536b45
aneeshpatne <aneeshpatne@gmail.com>
2024-10-10 13:57:13 +0530

    Moved many components to SSR

--- src/components/PhotoFrame.jsx
+++ src/components/PhotoFrame.jsx
@@ -1,9 +1,7 @@
-'use client';
-import { UseThemeContext } from './ThemeContext';
+
 import styles from './style/aboutPhotoFrame.module.css';
 import Image from 'next/image';
-export default function Photoframe(){
-    const {theme, ThemeToggle} = UseThemeContext();
+export default function Photoframe({theme}){
     return(<div id={styles.PhotoFrameContainer} className ={theme === 'LightMode' ? styles.LightMode : ''}>
         <div id={styles.FrameImgContainer}>
             <Image src='/assets/img/aneesh3.png' alt='its me' height={640} width={640} draggable={false} style={{ objectFit: 'contain' }}/>
