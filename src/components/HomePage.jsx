9ea8895be4d8bb8f9bb1ee3803790c2cd8abc482
aneeshpatne <aneeshpatne@gmail.com>
2025-01-18 16:58:23 +0530

    feat: pass theme prop to NewProject component and update its heading text

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -31,7 +31,7 @@ export default function HomePage() {
       <Aboutme theme={theme} />
       <LazyStats theme={theme} />
       <LazySkills />
-      <NewProject />
+      <NewProject theme={theme} />
     </>
   );
 }
