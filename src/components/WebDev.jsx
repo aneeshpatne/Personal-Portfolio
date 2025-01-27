300ba1492f3e6b524fe3f74d9e4d83150f77dd23
aneeshpatne <aneeshpatne@gmail.com>
2025-01-27 17:04:09 +0530

    feat: enhance WebDev component with styled text and responsive design adjustments

--- src/components/WebDev.jsx
+++ src/components/WebDev.jsx
@@ -2,7 +2,13 @@ import styles from "./style/WebDev.module.css";
 export default function WebDev() {
   return (
     <div className={styles.Container}>
-      <h1 className={styles.Text}>&lt;Web Development/&gt;</h1>
+      <h1 className={styles.Text}>
+        &lt;
+        <span className={styles.webWrapper}>
+          <span className={styles.web}>Web</span>
+        </span>{" "}
+        Development/&gt;
+      </h1>
     </div>
   );
 }
