e995aa847ef68b580755d3eb791973159e97f569
aneeshpatne <aneeshpatne@gmail.com>
2025-04-09 14:21:31 +0530

    feat: import Menu component in HomePage and remove dynamic import

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,7 +4,7 @@ import React from "react";
 import dynamic from "next/dynamic";
 import NameNew from "./NameNew";
 import Loading from "@/app/loading";
-
+import Menu from "./Menu";
 const Profile = dynamic(() => import("./AboutMeNew"), {
   loading: () => <Loading />,
 });
@@ -23,9 +23,7 @@ const DSA = dynamic(() => import("./DSA"), {
 const ProjectNew = dynamic(() => import("./ProjectNew"), {
   loading: () => <Loading />,
 });
-const Menu = dynamic(() => import("./menu"), {
-  loading: () => <Loading />,
-});
+
 export default function HomePage() {
   return (
     <>
