66b230b9fdbe88e6925e3ab177dc8fc4b80aff37
aneeshpatne <aneeshpatne@gmail.com>
2025-09-22 13:32:28 +0530

    feat: update profileContainer layout and styles for improved responsiveness and structure

--- src/components/profileContainer.jsx
+++ src/components/profileContainer.jsx
@@ -1,4 +1,12 @@
 import styles from "./style/profileContainer.module.css";
 export default function Container() {
-  return <div className={styles.mainContainer}> hello!</div>;
+  return (
+    <div className={styles.mainContainer}>
+      <div className={styles.Container}>
+        <div className={styles.techStackItem}></div>
+        <div className={styles.linkItem}></div>
+        <div className={styles.mainPageItem}></div>
+      </div>
+    </div>
+  );
 }
