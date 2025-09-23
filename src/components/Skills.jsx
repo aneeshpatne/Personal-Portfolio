02df5663e8fdd35b4ffac6ccf16afb5948018e83
aneeshpatne <aneeshpatne@gmail.com>
2025-09-23 22:16:52 +0530

    Implement code changes to enhance functionality and improve performance

--- src/components/Skills.jsx
+++ src/components/Skills.jsx
@@ -4,7 +4,6 @@ import styles from "./style/Skill.module.css";
 const Skills = () => {
   return (
     <div className={styles.container}>
-      {/* Eyebrow title with animated gradient lines */}
       <div className={styles.lineContainer}>
         <span className={`${styles.gradientLine} ${styles.left}`} />
         <span className={styles.eyebrow}>What I&apos;m Good At</span>
