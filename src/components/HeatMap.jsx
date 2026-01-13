9775e80e41ba9d90edd1db2d0af86cee15e72ca3
aneeshpatne <aneeshpatne@gmail.com>
2026-01-13 17:44:26 +0530

    feat: Replace HeatMapClient with ActivityCalendar and enhance styling for improved layout

--- src/components/HeatMap.jsx
+++ src/components/HeatMap.jsx
@@ -1,4 +1,12 @@
-import { HeatMapClient } from "./HeatMapClient";
+import { ActivityCalendar } from "react-activity-calendar";
+import styles from "./style/HeatMap.module.css";
+import { Fira_Code } from "next/font/google";
+
+const firaCode = Fira_Code({
+  subsets: ["latin"],
+  weight: ["400", "500"],
+  display: "swap",
+});
 
 export async function HeatMap() {
   const apiKey = process.env.ANEESH_API_KEY;
@@ -27,5 +35,32 @@ export async function HeatMap() {
     },
   ];
 
-  return <HeatMapClient data={data} />;
+  const theme = {
+    light: ["#ebedf0", "#9be9a8", "#40c463", "#30a14e", "#216e39"],
+    dark: ["#161b22", "#0e4429", "#006d32", "#26a641", "#39d353"],
+  };
+
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.calendarBox}>
+        <ActivityCalendar
+          data={data}
+          weekStart={1}
+          blockMargin={4}
+          blockRadius={2}
+          blockSize={12}
+          theme={theme}
+          colorScheme="dark"
+          showMonthLabels
+          showWeekdayLabels={["mon", "sun"]}
+        />
+      </div>
+      <p className={`${styles.caption} ${firaCode.className}`}>
+        <span className={styles.dot}></span>
+        <span className={styles.yearHighlight}>2026 So far</span>
+        <span className={styles.separator}>•</span>
+        Github, Leetcode, CSES, Codeforces
+      </p>
+    </div>
+  );
 }
