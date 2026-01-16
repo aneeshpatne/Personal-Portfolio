9e185f0db1a593122475a5ba6e7ccd84366a4c5f
aneeshpatne <aneeshpatne@gmail.com>
2026-01-16 13:15:50 +0530

    feat: enhance APIShowCase component to fetch and display PM25 data with value, alert color, and remark

--- src/components/APIShowCase.jsx
+++ src/components/APIShowCase.jsx
@@ -1,6 +1,25 @@
 import { PM25 } from "./PM25";
 import styles from "./style/APIShowCase.module.css";
 
-export function APIShowCase() {
-  return <PM25 />;
+export async function APIShowCase() {
+  const apiKey = process.env.ANEESH_API_KEY;
+
+  const response = await fetch("https://api.aneeshpatne.com/api/v1/pm25", {
+    headers: {
+      authorization: `Bearer ${apiKey}`,
+    },
+    next: {
+      revalidate: 86400,
+    },
+  });
+
+  const fetchedData = await response.json();
+
+  return (
+    <PM25
+      value={fetchedData?.value}
+      alertColor={fetchedData?.alert_color}
+      remark={fetchedData?.remark}
+    />
+  );
 }
