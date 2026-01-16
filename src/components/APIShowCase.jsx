e080cbee5f323d7c8496eea23c048f1e20b0693a
aneeshpatne <aneeshpatne@gmail.com>
2026-01-16 13:23:10 +0530

    feat: refactor APIShowCase and PM25 components to fetch PM25 data and handle fallback values for improved data display

--- src/components/APIShowCase.jsx
+++ src/components/APIShowCase.jsx
@@ -1,25 +1,5 @@
 import { PM25 } from "./PM25";
-import styles from "./style/APIShowCase.module.css";
 
-export async function APIShowCase() {
-  const apiKey = process.env.ANEESH_API_KEY;
-
-  const response = await fetch("https://api.aneeshpatne.com/api/v1/pm25", {
-    headers: {
-      authorization: `Bearer ${apiKey}`,
-    },
-    next: {
-      revalidate: 86400,
-    },
-  });
-
-  const fetchedData = await response.json();
-
-  return (
-    <PM25
-      value={fetchedData?.value}
-      alertColor={fetchedData?.alert_color}
-      remark={fetchedData?.remark}
-    />
-  );
+export function APIShowCase() {
+  return <PM25 />;
 }
