d28efc389867da4ed74c8dd103585171b2753ac4
aneeshpatne <aneeshpatne@gmail.com>
2026-01-16 12:05:40 +0530

    feat: enhance APIShowCase and PM25 components with value and remark props for improved data display

--- src/components/APIShowCase.jsx
+++ src/components/APIShowCase.jsx
@@ -2,5 +2,5 @@ import { PM25 } from "./PM25";
 import styles from "./style/APIShowCase.module.css";
 
 export function APIShowCase() {
-  return <PM25 />;
+  return <PM25 value={75} remark={"Hazardous Air Quality"} />;
 }
--- src/components/PM25.jsx
+++ src/components/PM25.jsx
@@ -1,5 +1,10 @@
 "use client";
 import styles from "./style/PM25.module.css";
 export function PM25({ value, remark }) {
-  return <div className={styles.container}></div>;
+  return (
+    <div className={styles.container}>
+      <div>{value}</div>
+      <div>{remark}</div>
+    </div>
+  );
 }
