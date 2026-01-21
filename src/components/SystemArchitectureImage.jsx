f7c0a453e73e68eef98f4e6e4aa0ab29a31906c1
aneeshpatne <aneeshpatne@gmail.com>
2026-01-21 16:39:42 +0530

    feat: add CCTVFlowSVG component and update SystemArchitectureImage for improved layout and styling

--- src/components/SystemArchitectureImage.jsx
+++ src/components/SystemArchitectureImage.jsx
@@ -1,5 +1,14 @@
 import style from "./style/SystemArchitectureImage.module.css";
-
+import Image from "next/image";
 export function SystemArchitectureImage() {
-  return <div className={style.ImageContainer}></div>;
+  return (
+    <div className={style.ImageContainer}>
+      <Image
+        src={"/assets/img/cam.png"}
+        width={350}
+        height={250}
+        alt="DIY CCTV Cam"
+      />
+    </div>
+  );
 }
