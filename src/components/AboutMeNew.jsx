8fe80db67706cbc81a5de3085bd148a497dfd04a
aneeshpatne <aneeshpatne@gmail.com>
2026-04-04 11:17:42 +0000

    feat: update font to Inter and enhance About Me section layout for improved readability and aesthetics

--- src/components/AboutMeNew.jsx
+++ src/components/AboutMeNew.jsx
@@ -1,10 +1,28 @@
 import styles from "./style/newAboutMe.module.css";
-
+import { Lexend } from "next/font/google";
+const lexend = Lexend({
+  subsets: ["latin"],
+  weight: ["300", "400", "500", "600", "700"],
+  display: "swap",
+});
 export default function Profile() {
   return (
     <section className={styles.profileSection} aria-labelledby="about-heading">
       <div className={styles.inner}>
-        <div className={styles.Text}></div>
+        <div className={styles.textContainer}>
+          <div className={`${styles.text} ${lexend.className}`}>
+            <span>Ideas</span>
+            <span>Into</span>
+            <span className={styles.secondaryText}>Reality</span>
+          </div>
+          <div className={styles.finePrint}>
+            <span className={styles.finePrintLabel}>What I do</span>
+            <span>
+              I build at the intersection of AI, system design, and product
+              thinking, transforming ambiguity into impact.
+            </span>
+          </div>
+        </div>
         <div className={styles.polaroidWrapper}>
           <figure className={styles.polaroid}>
             <img
